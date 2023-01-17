76e629d576fba9e7400082dd416577cb83a82720
Dorota Palmąka <dorotapalmaka@wp.pl>
2023-01-17 10:48:05 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+{
+  const tasks = [
+    {
+        content: "nagrać lekcję",
+        done: false,
+    },
+    {
+        content: "zjeść pierogi",
+        done: true,
+
+    },
+  ];
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+        htmlString += `
+        <li>
+         ${task.content}
+        </li>
+        `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
+
+  const init = () => {
+    render();
+  };
+
+  init();
+}
