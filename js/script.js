615719ed2424293612a80c5bf062bc6a71e5f884
Dorota Palmąka <dorotapalmaka@wp.pl>
2023-01-18 08:24:10 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,23 +10,6 @@
     },
   ];
 
-  const render = () => {
-    let htmlString = "";
-
-    for (const task of tasks) {
-      htmlString += `
-        <li
-        ${task.done ? ' style="text-decoration: line-through"' : ""}
-        >
-        <button>Usuń</button>
-        ${task.content}
-        </li>
-        `;
-    }
-
-    document.querySelector(".js-tasks").innerHTML = htmlString;
-  };
-
   const addNewTask = (newTaskContent) => {
     tasks.push({
       content: newTaskContent,
@@ -35,6 +18,37 @@
     render();
   };
 
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+        <li
+        ${task.done ? ' style="text-decoration: line-through"' : ""}
+        >
+        <button class="js-remove">Usuń</button>
+        ${task.content}
+        </li>
+        `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+  };
+
+  
   const onFormSubmit = (event) => {
     event.preventDefault();
 
